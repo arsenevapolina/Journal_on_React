fee6cd5643ef92f76955f6441b3340d0d2cd57b1
Полина <pechenkova.polina.mgu@yandex.ru>
2024-09-29 05:02:58 +0300

    Реализована отправка формы

---
+++ react-app/src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import CardButton from "./components/CardButton/CardButton";
+import Header from "./components/Header/Header";
+import JournalItem from "./components/JournalItem/JournalItem";
+import JournalList from "./components/JournalList/JournalList";
+import Body from "./layouts/Body/Body";
+import LeftPanel from "./layouts/LeftPanel/LeftPanel";
+import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
+import JournalForm from "./components/JournalForm/JournalForm";
+
+function App() {
+  const data = [
+    {
+      title: "Подготовка к обновлению курсов",
+      text: "Горные походы открывают удивительный природный ландшафт.",
+      date: new Date(),
+    },
+    {
+      title: "Поход в горы",
+      text: "Думал, что очень много времени",
+      date: new Date(),
+    },
+  ];
+
+  return (
+    <div className="app">
+      <LeftPanel>
+        <Header />
+        <JournalAddButton />
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              text={data[0].text}
+              date={data[0].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              text={data[1].text}
+              date={data[1].date}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>
+        <JournalForm />
+      </Body>
+    </div>
+  );
+}
+
+export default App;
